340efcf40d3a8e10b00fd7ede968be651a9f978f
Gustavo <gustavomarquezmendieta@gmail.com>
2024-09-04 19:09:24 -0400

    Se agrego nueva carpeta agregando los avances

---
+++ Proyecto/application/views/bebidas.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Inicio</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css">
+    <link href="<?php echo base_url(); ?>assets/css/cambios.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/cards.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+    <!-- Vendor CSS Files -->
+    <link href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/vendor/aos/aos.css" rel="stylesheet">
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            overflow-x: hidden;
+        }
+
+        li.user-info {
+            list-style-type: none;
+            padding-left: 0;
+        }
+
+        .dashboard-sideBar {
+            left: 0;
+            z-index: 2;
+            background-image: url('../../assets/img/copa.jpg');
+            background-color: #333;
+            color: white;
+            width: 250px;
+            position: fixed;
+            height: 100%;
+            overflow-y: auto;
+        }
+
+        .dashboard-sideBar a {
+            display: block;
+            padding: 10px 20px;
+            color: white;
+            text-decoration: none;
+        }
+
+        .dashboard-sideBar a:hover {
+            background-color: #555;
+        }
+
+        .dashboard-sideBar .btn-sideBar-SubMenu {
+            padding-left: 30px;
+            position: relative;
+        }
+
+        .dashboard-sideBar .btn-sideBar-SubMenu::after {
+            content: "\f0d7";
+            font-family: "Material Icons";
+            position: absolute;
+            right: 20px;
+        }
+
+        .dashboard-contentPage {
+            margin-left: 250px;
+            padding: 20px;
+            position: relative;
+            height: auto;
+            overflow-y: auto;
+        }
+
+        .dashboard-sideBar img {
+            width: 24px;
+            height: 24px;
+            margin-right: 10px;
+            vertical-align: middle;
+        }
+
+        .frase {
+            font-size: 40px;
+            font-weight: bold;
+            margin-top: 20px;
+            margin-left: 30px;
+            background-image: linear-gradient(45deg, #FF6B6B, #87CEEB);
+            -webkit-background-clip: text;
+            background-clip: text;
+            color: transparent;
+        }
+
+        .titulo {
+            font-size: 60px;
+            font-weight: bold;
+            margin-top: 20px;
+            margin-left: 30px;
+            background-image: linear-gradient(45deg, #FF6B6B, #87CEEB);
+            -webkit-background-clip: text;
+            background-clip: text;
+            color: transparent;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- SideBar -->
+    <section class="full-box cover dashboard-sideBar">
+        <div class="full-box dashboard-sideBar-bg btn-menu-dashboard"></div>
+        <div class="full-box dashboard-sideBar-ct">
+            <div class="full-box text-uppercase text-center text-titles dashboard-sideBar-title">
+                EL DETALLE <i class="zmdi zmdi-close btn-menu-dashboard visible-xs"></i>
+            </div>
+            <div class="full-box dashboard-sideBar-UserInfo">
+                <figure class="full-box">
+                    <img src="../../assets/img/StudetMaleAvatar.png" alt="UserIcon"> <!-- Imagen de avatar -->
+                    <figcaption class="text-center text-titles">
+                        <li class="user-info">
+                            <h6>Bienvenido</h6>
+                            <span class="user-name" style="font-size:20px"> <?= $nombre; ?></span>
+                        </li>
+                    </figcaption>
+                </figure>
+
+                <ul class="full-box list-unstyled text-center">
+                    <li>
+                        <a href="<?php echo site_url('Welcome/inicio'); ?>" title="Inicio" class="btn-user">
+                            <img src="../../assets/img/hogar.png">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Mi Informacion" class="btn-user">
+                            <img src="../../assets/img/avatar (1).png">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo site_url('Welcome/configuracion'); ?>" title="Configuracion">
+                            <img src="../../assets/img/configuracion-de-usuario.png">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?php echo site_url('Welcome/cerrarsesion'); ?>" title="Salir del sistema"
+                            class="btn-exit-system">
+                            <img src="../../assets/img/cerrar-sesion.png">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- SideBar Menu -->
+            <ul class="list-unstyled full-box dashboard-sideBar-Menu">
+                <li>
+                    <a href="<?php echo site_url('Welcome/vajilla'); ?>">
+                        <img src="../../assets/img/copa-con-vino.png" alt="Vajilla"> Vajilla
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo site_url('Welcome/manteleria'); ?>">
+                        <img src="../../assets/img/mesa.png" alt="Mantelería y Decoración"> Mantelería y Decoración
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo site_url('Welcome/bebidas'); ?>">
+                        <img src="../../assets/img/vino.png" alt="Bebidas"> Bebidas
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo site_url('Welcome/carrito'); ?>">
+                        <img src="../../assets/img/carrito-de-compras.png" alt="Carrito"> Servicios a adquirir
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <!-- Content page -->
+    <!-- Content page -->
+    <section class="full-box dashboard-contentPage" id="inicio">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <br>
+                    <h1 class="titulo">"BEBIDAS"</h1>
+                </div>
+                <div class="col-md-8">
+                    <?php foreach ($productos as $row) { ?>
+                        <div class="col-12 mt-5 Products">
+                            <div class="card d-flex flex-row"
+                                style="max-height: 250px; min-height: 250px; background-color:rgba(45, 66, 66, 0.199);">
+                                <div>
+                                    <img class="card-img-left"
+                                        src="<?php echo base_url('./assets/img/' . $row['imagen']); ?>"
+                                        alt="<?php echo $row['nombre']; ?>"
+                                        style="width: 200px; height: 200px; object-fit: cover;">
+                                </div>
+                                <div class="card-body text-left">
+                                    <h5 class="card-title"><?php echo $row['nombre']; ?></h5>
+                                    <ul class="card-text">
+                                        <?php
+                                        $ingredientes = explode("\n", $row['detalles']);
+                                        foreach ($ingredientes as $ingrediente) {
+                                            echo '<li>' . htmlspecialchars(trim($ingrediente)) . '</li>';
+                                        }
+                                        ?>
+                                    </ul>
+                                    <a href="<?php echo site_url('decoracion/detalle/' . $row['bebida_id']); ?>"
+                                        class="red_button btn_puntos" title="Ver <?php echo $row['nombre']; ?>">
+                                        Ingredientes
+                                        <i class="bi bi-arrow-right-circle"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+
+
+
+            </div>
+        </div>
+    </section>
+
+
+    <!--====== Scripts -->
+    <script src="./js/jquery-3.1.1.min.js"></script>
+    <script src="./js/sweetalert2.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/material.min.js"></script>
+    <script src="./js/ripples.min.js"></script>
+    <script src="./js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="./js/main.js"></script>
+    <script>
+        $.material.init();
+    </script>
+</body>
+
+</html>
